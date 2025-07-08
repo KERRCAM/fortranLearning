f10a504b10874146ae0b89b399bd8d7e2108358b
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-07-08 16:30:42 +0100

    optional parameters

--- functions.f90
+++ functions.f90
@@ -11,13 +11,21 @@ program functions
 
     contains
 
-        function pow2(x) result(output)
+        function pow2(x, flag) result(output)
             implicit none
 
             ! intent(in) -> read only, like C++ const, out for write only, inout for read write
-            ! only in should be used in a function to keep it pure, use a sub routine if writes are needed on a parameter
+            ! only in should be used in a function to keep it pure, use a sub routine if writes are needed on a argument
             integer, intent(in) :: x
             integer :: output
+
+            ! optional parameter means the function doesn't always need this argument (can be used on sub routines too)
+            character, intent(in), optional :: flag
+
+            ! You cna check if an optional parameter has been given with present
+            if (present(flag)) then
+                print *, 'Optional flag present'
+            end if
 
             output = 2 ** x
 
